91897b61c35027a2bd7d963787c0252ef8372d42
Konstantin Koulechov <kostek@sni.pl>
2020-01-23 15:50:55 +0100

    Changed default setAcceptCreditCards & setPayPalShippingAddressOption options. TODO: give user chance to change it

--- ios/RNPaypalWrapper.m
+++ ios/RNPaypalWrapper.m
@@ -19,11 +19,11 @@ NSString * const USER_CANCELLED = @"USER_CANCELLED";
 - (NSDictionary *)constantsToExport
 {
     return @{
-             @"PRODUCTION": PayPalEnvironmentProduction,
-             @"SANDBOX": PayPalEnvironmentSandbox,
-             @"NO_NETWORK": PayPalEnvironmentNoNetwork,
-             USER_CANCELLED: USER_CANCELLED
-             };
+        @"PRODUCTION": PayPalEnvironmentProduction,
+        @"SANDBOX": PayPalEnvironmentSandbox,
+        @"NO_NETWORK": PayPalEnvironmentNoNetwork,
+        USER_CANCELLED: USER_CANCELLED
+    };
 }
 
 RCT_EXPORT_MODULE();
@@ -43,9 +43,9 @@ RCT_EXPORT_METHOD(initializeWithOptions:(NSString *) environment clientId:(NSStr
         [PayPalMobile preconnectWithEnvironment:environment];
     });
 
-    if([options objectForKey:@"merchantName"] != nil 
-        && [options objectForKey:@"merchantPrivacyPolicyUri"] != nil 
-        && [options objectForKey:@"merchantUserAgreementUri"] != nil ) {
+    if([options objectForKey:@"merchantName"] != nil
+       && [options objectForKey:@"merchantPrivacyPolicyUri"] != nil
+       && [options objectForKey:@"merchantUserAgreementUri"] != nil ) {
 
         NSString *merchantName = [RCTConvert NSString:options[@"merchantName"]];
         NSString *merchantPrivacyPolicyUri = [RCTConvert NSString:options[@"merchantPrivacyPolicyUri"]];
@@ -75,6 +75,7 @@ RCT_EXPORT_METHOD(obtainConsent:(RCTPromiseResolveBlock)resolve
     self.reject = reject;
 
     PayPalFuturePaymentViewController *vc = [[PayPalFuturePaymentViewController alloc] initWithConfiguration:self.configuration delegate:self];
+    vc.modalPresentationStyle = UIModalPresentationFullScreen;
 
     // Present the PayPalFuturePaymentViewController
     UIViewController *visibleVC = [[[UIApplication sharedApplication] keyWindow] rootViewController];
@@ -95,24 +96,25 @@ RCT_EXPORT_METHOD(pay:(NSDictionary *)options resolver:(RCTPromiseResolveBlock)r
 {
     self.resolve = resolve;
     self.reject = reject;
-    
+
     NSString *price = [RCTConvert NSString:options[@"price"]];
     NSString *currency = [RCTConvert NSString:options[@"currency"]];
     NSString *description = [RCTConvert NSString:options[@"description"]];
-    
+
     self.payment = [[PayPalPayment alloc] init];
     [self.payment setAmount:[[NSDecimalNumber alloc] initWithString:price]];
     [self.payment setCurrencyCode:currency];
     [self.payment setShortDescription:description];
-    
+
     self.configuration = [[PayPalConfiguration alloc] init];
-    [self.configuration setAcceptCreditCards:true];
-    [self.configuration setPayPalShippingAddressOption:PayPalShippingAddressOptionPayPal];
-    
+    [self.configuration setAcceptCreditCards:false];
+    [self.configuration setPayPalShippingAddressOption:PayPalShippingAddressOptionProvided];
+
     PayPalPaymentViewController *vc = [[PayPalPaymentViewController alloc] initWithPayment:self.payment
-                                                                             configuration:self.configuration
-                                                                                  delegate:self];
-    
+                                       configuration:self.configuration
+                                       delegate:self];
+
+    vc.modalPresentationStyle = UIModalPresentationFullScreen;
     UIViewController *visibleVC = [[[UIApplication sharedApplication] keyWindow] rootViewController];
     do {
         if ([visibleVC isKindOfClass:[UINavigationController class]]) {
@@ -133,43 +135,43 @@ RCT_EXPORT_METHOD(pay:(NSDictionary *)options resolver:(RCTPromiseResolveBlock)r
 - (void)payPalPaymentDidCancel:(PayPalPaymentViewController *)paymentViewController
 {
     [paymentViewController.presentingViewController dismissViewControllerAnimated:YES completion:^{
-        if (self.reject) {
-            NSError *error = [NSError errorWithDomain:RCTErrorDomain code:1 userInfo:NULL];
-            self.reject(USER_CANCELLED, USER_CANCELLED, error);
-        }
-    }];
+         if (self.reject) {
+             NSError *error = [NSError errorWithDomain:RCTErrorDomain code:1 userInfo:NULL];
+             self.reject(USER_CANCELLED, USER_CANCELLED, error);
+         }
+     }];
 }
 
 - (void)payPalPaymentViewController:(PayPalPaymentViewController *)paymentViewController
-                 didCompletePayment:(PayPalPayment *)completedPayment
+    didCompletePayment:(PayPalPayment *)completedPayment
 {
     [paymentViewController.presentingViewController dismissViewControllerAnimated:YES completion:^{
-        if (self.resolve) {
-            self.resolve(completedPayment.confirmation);
-        }
-    }];
+         if (self.resolve) {
+             self.resolve(completedPayment.confirmation);
+         }
+     }];
 }
 
 #pragma mark - PayPalFuturePaymentDelegate methods
 
 - (void)payPalFuturePaymentDidCancel:(PayPalFuturePaymentViewController *)futurePaymentViewController {
-  // User cancelled login. Dismiss the PayPalLoginViewController, breathe deeply.
+    // User cancelled login. Dismiss the PayPalLoginViewController, breathe deeply.
     [futurePaymentViewController.presentingViewController dismissViewControllerAnimated:YES completion:^{
-        if (self.reject) {
-            NSError *error = [NSError errorWithDomain:RCTErrorDomain code:1 userInfo:NULL];
-            self.reject(USER_CANCELLED, USER_CANCELLED, error);
-        }
-    }];
+         if (self.reject) {
+             NSError *error = [NSError errorWithDomain:RCTErrorDomain code:1 userInfo:NULL];
+             self.reject(USER_CANCELLED, USER_CANCELLED, error);
+         }
+     }];
 }
 
 - (void)payPalFuturePaymentViewController:(PayPalFuturePaymentViewController *)futurePaymentViewController
-                didAuthorizeFuturePayment:(NSDictionary *)futurePaymentAuthorization {
+    didAuthorizeFuturePayment:(NSDictionary *)futurePaymentAuthorization {
     // Be sure to dismiss the PayPalLoginViewController.
     [futurePaymentViewController.presentingViewController dismissViewControllerAnimated:YES completion:^{
-        if (self.resolve) {
-            self.resolve(futurePaymentAuthorization);
-        }
-    }];
+         if (self.resolve) {
+             self.resolve(futurePaymentAuthorization);
+         }
+     }];
 }
 
 + (BOOL)requiresMainQueueSetup {
